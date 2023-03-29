f9dcd5b953adda0b7c169e2aee4869b075e1f577
jeongseop-kim-ringle <jeongseop.kim@ringleplus.com>
2023-03-29 10:11:04 +0900

    add out

---
+++ my-app/out/_next/static/kn5KOlnSDSKpVkhi4Slsj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0a1d46e34c493d1d.css","static/chunks/pages/index-a369a9f5a025b0e8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
