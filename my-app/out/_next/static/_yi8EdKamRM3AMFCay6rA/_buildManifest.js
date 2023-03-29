c1a670fd4de090fa53ae9d40e7bfbbe2da50f060
jeongseop-kim-ringle <jeongseop.kim@ringleplus.com>
2023-03-29 13:14:28 +0900

    deploy to main

--- my-app/out/_next/static/_yi8EdKamRM3AMFCay6rA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4f0429bc2140661b.css","static/chunks/pages/index-494a252ba6f62581.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ my-app/out/_next/static/xe2JKwNLM2yzSrsm1Tyxg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ea8b3cedf795a207.css","static/chunks/pages/index-b03d12367cc9c2bb.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
