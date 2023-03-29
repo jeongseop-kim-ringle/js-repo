03ac2b3036c9c30a6e84b2ba0c9d4e30787c0a68
jeongseop-kim-ringle <jeongseop.kim@ringleplus.com>
2023-03-29 10:03:40 +0900

    cofig

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   reactStrictMode: true,
   assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  images: {
+    loader: "custom",
+    loaderFile: "./image/loader.js",
+  },
 };
 
 module.exports = nextConfig;
